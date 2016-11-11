aaaeeba09dc291d9228142835533da21a80f712e
juven1996@gmail.com <juven1996@gmail.com>
2016-11-10 20:03:21 -0800

    changed logo etc

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -4,30 +4,7 @@
 <?php echo $welcome_message; ?>
 
 <div class="clearfix"></div>
-<hr />
 
 
 
-<p><?php echo lang('welcome content ci_docs'); ?></p>
 
-<div class="clearfix"><hr /></div>
-
-<p><a href="<?php echo base_url('api/users'); ?>"><?php echo lang('welcome content click_here'); ?></a>: <?php echo lang('welcome content sample_api'); ?></p>
-
-<div class="clearfix"><hr /></div>
-
-<p><a href="<?php echo base_url('profile'); ?>"><?php echo lang('welcome content click_here'); ?></a>: <?php echo lang('welcome content sample_profile'); ?></p>
-<p>
-    <?php echo lang('welcome content username'); ?>: <strong>johndoe</strong><br />
-    <?php echo lang('welcome content or_email'); ?>: <strong>john@doe.com</strong><br />
-    <?php echo lang('welcome content password'); ?>: <strong>johndoe</strong>
-</p>
-
-<div class="clearfix"><hr /></div>
-
-<p><a href="<?php echo base_url('admin'); ?>"><?php echo lang('welcome content click_here'); ?></a>: <?php echo lang('welcome content sample_admin'); ?></p>
-<p>
-    <?php echo lang('welcome content username'); ?>: <strong>admin</strong><br />
-    <?php echo lang('welcome content or_email'); ?>: <strong>admin@admin.com</strong><br />
-    <?php echo lang('welcome content password'); ?>: <strong>admin</strong>
-</p>
